4f27ac617eed576ff92375ed0e3b8734254e2ea4
rpererah <rpererah@gmail.com>
2024-06-06 20:42:41 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+
+export default function App(){
+  return(
+    <div>
+      <Button variant="contained" color="primary">
+        Hello Mui
+      </Button>
+    </div>
+  )
+}
